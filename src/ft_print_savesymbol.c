7ce72e3110c744ef96e8450843185c84363673e0
ttanja <ttanja@student.42.fr>
2021-06-10 14:15:31 +0300

    i am going to school

--- src/ft_print_savesymbol.c
+++ src/ft_print_savesymbol.c
@@ -6,27 +6,31 @@
 /*   By: ttanja <pamishenko@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/28 20:21:02 by ttanja            #+#    #+#             */
-/*   Updated: 2021/06/10 07:35:14 by ttanja           ###   ########.fr       */
+/*   Updated: 2021/06/10 14:15:17 by ttanja           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-unsigned long long	ft_print_savesymbol(t_flags **flag)
+long long	ft_print_savesymbol(t_flags **flag)
 {
 	char				r;
-	unsigned long long	result;
+	long long	result;
 
-	(*flag)->width = (*flag)->width - (*flag)->dot;
-	result = (*flag)->width;
+	if ((((*flag)->width) - ((*flag)->dot)) > 0)
+		(*flag)->width = ((*flag)->width) - ((*flag)->dot);
+	else
+		(*flag)->width = 0;
+	result = (*flag)->width;VYVVdsfb 
 	if ((*flag)->zero == 1 && (*flag)->conversion != POINTER)
 		r = '0';
 	else
 		r = ' ';
-	while ((*flag)->width > 1)
+	while (((*flag)->width) > 0)
 	{
 		ft_putchar_fd(r, 1);
 		(*flag)->width = (*flag)->width - 1;
+		printf("\n what is it????  %lld\n", (*flag)->width);
 	}
 	return (result);
 }
